73bef92f27467b102bb4cc85bb295e8dcad44491
Serhii Kravchenko <kravchenko087@ukr.net>
2022-08-29 13:24:09 +0300

    сделал рефакторинг Searchbar на Хуки

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,58 +1,53 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import { ImSearch } from 'react-icons/im'
 import { toast } from 'react-toastify'
 import PropTypes from 'prop-types'
 import { Header, Form, FormButton, FormInput } from './Searchbar.styled'
 
-export class Searchbar extends Component {
-    static propTypes = {
-        onSubmit: PropTypes.func.isRequired,
-    }
-    state = {
-        name: "",
+export const Searchbar = ({ onSubmit }) => {
+    const [name, setName] = useState('');
+
+    const handleNameChange = (e) => {
+        setName(e.target.value)
     }
 
-    handleNameChange = (e) => {
-        this.setState({ name: e.target.value })
-    }
-
-    submitForm = (e) => {
+    const submitForm = (e) => {
         e.preventDefault()
-        if (this.state.name.trim() === '') {
+
+        if (name.trim() === '') {
             toast.warn("Please enter a request", {
                 theme: "colored",
             })
         }
 
-        this.props.onSubmit(this.state.name);
-        this.reset()
+        onSubmit(name)
+        setName("")
     }
 
-    reset() {
-        this.setState({ name: "" })
-    }
+    return (
+        <Header className="searchbar">
+            <Form className="form" onSubmit={submitForm}>
+                <FormButton type="submit" className="button">
+                    <ImSearch style={{ width: 20, height: 20 }} />
+                </FormButton>
 
-    render() {
-        return (
-            <Header className="searchbar">
-                <Form className="form" onSubmit={this.submitForm}>
-                    <FormButton type="submit" className="button">
-                        <ImSearch style={{ width: 20, height: 20 }} />
-                    </FormButton>
-
-                    <FormInput
-                        className="input"
-                        type="text"
-                        autoComplete="off"
-                        autoFocus
-                        placeholder="Search images and photos"
-                        value={this.state.name}
-                        onChange={this.handleNameChange}
-                    />
-                </Form>
-            </Header>
-        )
-    }
+                <FormInput
+                    className="input"
+                    type="text"
+                    autoComplete="off"
+                    autoFocus
+                    placeholder="Search images and photos"
+                    value={name}
+                    onChange={handleNameChange}
+                />
+            </Form>
+        </Header>
+    )
 }
 
+
 export default Searchbar
+
+Searchbar.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+}
